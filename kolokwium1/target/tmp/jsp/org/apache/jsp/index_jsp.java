3c6c795fea2683b4dce5739ae54ecd9d77b76ea0
krzysztoftrusilo <43780500+krzysztoftrusilo@users.noreply.github.com>
2018-11-16 12:32:27 +0100

    Create index_jsp.java

---
+++ kolokwium1/target/tmp/jsp/org/apache/jsp/index_jsp.java
@@ -0,0 +1,126 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.Vector _jspx_dependants;
+
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_if_test;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public Object getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _jspx_tagPool_c_if_test = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+  }
+
+  public void _jspDestroy() {
+    _jspx_tagPool_c_if_test.release();
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html; charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("    \n");
+      out.write("\n");
+      out.write("\t\n");
+      out.write("\t<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n");
+      out.write("\t\n");
+      out.write("\t<html>\n");
+      out.write("\t<head>\n");
+      out.write("\t<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("\t<title>RODO Index</title>\n");
+      out.write("\t</head>\n");
+      out.write("\t<body>\n");
+      out.write("\t\t\t\t<h1>Polityka RODO</h1>\n");
+      out.write("\t\t\t\t<h3>Pola obowiazkowe do kontynuacji</h3>\n");
+      out.write("\t\t\t\t<form action=\"rodo\" method=\"get\">\n");
+      out.write("\t\t\t\t<input type=\"checkbox\" name=\"firstRodo\"> Zgadzam sie z polityka RODO <br/>\n");
+      out.write("\t\t\t\t<input type=\"checkbox\" name=\"secondRodo\"> Rozumiem i zgadzam sie na warunki umowy <br/>\n");
+      out.write("\t\t\t\t<br/>\n");
+      out.write("\t\t\t\t<h3>Pola opcjonalne, ale zalecane</h3>\n");
+      out.write("\t\t\t\t<input type=\"checkbox\" name=\"newsletter\"> Zgadzam sie uczestniczyc w newsletter\n");
+      out.write("\t\t\t\t<br/>\n");
+      out.write("\t\t\t\t\n");
+      out.write("\t\t\t\t\n");
+      out.write("\t\t\t\t");
+      if (_jspx_meth_c_if_0(_jspx_page_context))
+        return;
+      out.write("\n");
+      out.write("\t\t\t\t\n");
+      out.write("\t\t\t\t<button style=\"margin: 10px\" type=\"submit\">Wyslij</button>\n");
+      out.write("\t\t\t\t</form>\n");
+      out.write("\t</body>\n");
+      out.write("\t</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+
+  private boolean _jspx_meth_c_if_0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:if
+    org.apache.taglibs.standard.tag.rt.core.IfTag _jspx_th_c_if_0 = (org.apache.taglibs.standard.tag.rt.core.IfTag) _jspx_tagPool_c_if_test.get(org.apache.taglibs.standard.tag.rt.core.IfTag.class);
+    _jspx_th_c_if_0.setPageContext(_jspx_page_context);
+    _jspx_th_c_if_0.setParent(null);
+    _jspx_th_c_if_0.setTest(((java.lang.Boolean) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${sessionScope.rodo eq 'off'}", java.lang.Boolean.class, (PageContext)_jspx_page_context, null)).booleanValue());
+    int _jspx_eval_c_if_0 = _jspx_th_c_if_0.doStartTag();
+    if (_jspx_eval_c_if_0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+      do {
+        out.write("\n");
+        out.write("\t\t\t\t\t<p>Musisz zaakceptowac polityke RODO!</p>\n");
+        out.write("\t\t\t\t");
+        int evalDoAfterBody = _jspx_th_c_if_0.doAfterBody();
+        if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+          break;
+      } while (true);
+    }
+    if (_jspx_th_c_if_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_c_if_test.reuse(_jspx_th_c_if_0);
+      return true;
+    }
+    _jspx_tagPool_c_if_test.reuse(_jspx_th_c_if_0);
+    return false;
+  }
+}
