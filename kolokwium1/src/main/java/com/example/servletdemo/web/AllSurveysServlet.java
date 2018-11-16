15ed287d5c960bc8df0c9d5f37fe9f18ca226e4f
krzysztoftrusilo <43780500+krzysztoftrusilo@users.noreply.github.com>
2018-11-16 13:04:07 +0100

    Update AllSurveysServlet.java

--- kolokwium1/src/main/java/com/example/servletdemo/web/AllSurveysServlet.java
+++ kolokwium1/src/main/java/com/example/servletdemo/web/AllSurveysServlet.java
@@ -17,6 +17,36 @@ import javax.servlet.http.HttpServletResponse;
 import com.example.servletdemo.domain.Survey;
 import com.example.servletdemo.service.SurveyService;
 
+@WebServlet(urlPatterns = "/all-surveys")
 public class AllSurveysServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		SurveyService ss = (SurveyService) getServletContext().getAttribute("survey_service");
+	
+		List<Survey> allWardrobes = ss.getAllSurveys();
 
+		out.append("<html><body><h2>All wardrobes:</h2>");
+
+		for (Survey survey: allSurveys) {
+			out.append("<p>From: " + survey.getFrom() + "</p>");
+			out.append("<p>To: " + survey.getTo() + "</p>");
+			out.append("<p>Weight: " + survey.getFrequency() + "</p>");
+			out.append("<p>Wooden: " + survey.isAttention() + "</p>");
+		}
+
+		out.append( "</br><li><a href='index'>Index</a></li>");
+		out.append("</body></html>");
+		out.close();
+	}
+	
+	@Override
+	public void init() throws ServletException {
+
+		// application context
+		getServletContext().setAttribute("survey_service", new SurveyService());
+	}
 }
