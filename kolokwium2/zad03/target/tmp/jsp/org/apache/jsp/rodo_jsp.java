da7f9ee656202c1bfde92473990136bc776397b4
krzysztoftrusilo <43780500+krzysztoftrusilo@users.noreply.github.com>
2018-12-20 18:16:24 +0100

    Create rodo_jsp.java

---
+++ kolokwium2/zad03/target/tmp/jsp/org/apache/jsp/rodo_jsp.java
@@ -0,0 +1,71 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class rodo_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.Vector _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public Object getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html; charset=ISO-8859-1");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\r\n");
+      out.write("<!DOCTYPE html>\r\n");
+      out.write("<html>\r\n");
+      out.write("<head>\r\n");
+      out.write("<meta charset=\"ISO-8859-1\">\r\n");
+      out.write("<title>Warunki korzystania z serwisu</title>\r\n");
+      out.write("</head>\r\n");
+      out.write("\t<body>\r\n");
+      out.write("\t<form action=\"index.jsp\" method=\"post\">\r\n");
+      out.write("\t\tZgoda na przetwarzanie danych osobowych <input type=\"checkbox\" name=\"rodo\" required/></br>\r\n");
+      out.write("\t\tZgoda na przestrzeganie regulaminu <input type=\"checkbox\" name=\"rodo2\" required/></br>\r\n");
+      out.write("\t\tCzy chcesz zapisac sie na newsletter? <input type=\"checkbox\" name=\"newsletter\"/></br>\r\n");
+      out.write("\t<input type='submit' value='Accept'/>\r\n");
+      out.write("\t</body>\r\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
