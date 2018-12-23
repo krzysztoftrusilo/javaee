43cbd9675c38e44faa05aa933b73db353cbfd9b3
krzysztoftrusilo <43780500+krzysztoftrusilo@users.noreply.github.com>
2018-12-23 18:52:27 +0100

    Create WardrobeManager.java

---
+++ zad07/src/main/java/ug/ktrusilo/javaee/REST/service/WardrobeManager.java
@@ -0,0 +1,37 @@
+package ug.ktrusilo.javaee.REST.service;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import ug.ktrusilo.javaee.REST.domain.Wardrobe;
+
+@Stateless
+public class WardrobeManager {
+
+	@PersistenceContext()
+	EntityManager em;
+	
+	public void addWardrobe(Wardrobe wardrobe)
+	{
+		em.persist(wardrobe);
+	}
+	
+	public void deleteWardrobe(Wardrobe wardrobe)
+	{
+		em.remove(wardrobe);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Wardrobe> getAll()
+	{
+		return em.createNamedQuery("processor.all").getResultList();
+	}
+	
+	public Wardrobe getById(long id)
+	{
+		return em.find(Wardrobe.class, id);
+	}
+}
