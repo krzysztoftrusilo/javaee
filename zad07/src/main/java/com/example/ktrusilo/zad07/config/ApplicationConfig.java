759503755e2dfc34f6b1f2c24f6af039e7414249
krzysztoftrusilo <43780500+krzysztoftrusilo@users.noreply.github.com>
2019-02-05 22:18:15 +0100

    Create ApplicationConfig.java

---
+++ zad07/src/main/java/com/example/ktrusilo/zad07/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.example.ktrusilo.zad07.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class ApplicationConfig extends Application {
+}
+
