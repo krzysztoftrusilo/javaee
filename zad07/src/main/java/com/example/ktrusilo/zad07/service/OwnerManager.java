fc414e0438209b7cb81975ef665d54222eff8281
krzysztoftrusilo <43780500+krzysztoftrusilo@users.noreply.github.com>
2019-02-05 22:18:53 +0100

    Create OwnerManager.java

---
+++ zad07/src/main/java/com/example/ktrusilo/zad07/service/OwnerManager.java
@@ -0,0 +1,52 @@
+package com.example.ktrusilo.zad07.service;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.example.ktrusilo.zad07.domain.Owner;
+
+@Stateless
+public class OwnerManager {
+	
+	 @PersistenceContext
+	    EntityManager em;
+	    
+	    
+	    public void addOwner(Owner owner) {
+	        em.persist(owner);
+	    }
+
+	    public Owner updateOwner(Owner owner, int id) {
+	    	owner.setId(id);
+	    	Owner old = getOwner(id);
+	        if(old != null) {
+	            em.merge(owner);
+	        }
+	        return old;
+	    }
+
+	    public void deleteOwner(int id) {
+	    	Owner owner = getOwner(id);
+	        if(owner != null) {
+	            em.remove(owner);
+	        }
+	    }
+
+	    public Owner getOwner(int id) {
+	        return em.find(Owner.class, id);
+	    }
+
+	    @SuppressWarnings("unchecked")
+	    public List<Owner> getAllOwners() {
+	        return em.createNamedQuery("owner.getAll").getResultList();
+	    }
+
+
+	    public void deleteAllOwners(){
+	        em.createNamedQuery("owner.deleteAll").executeUpdate();
+	    }   
+
+}
